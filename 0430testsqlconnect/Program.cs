bed0489091b9e9ba40dd7b3dd1a2ab2bb82407ec
iven <iven840320@gmail.com>
2024-04-30 09:25:14 +0800

    加入專案檔案。

---
+++ 0430testsqlconnect/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace THM104_G2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Northwind;Integrated Security=True";
+            SqlConnection cn = new SqlConnection(connectionString);
+            cn.Open();
+            string query = "select * from customers";
+            SqlCommand cmd = new SqlCommand(query, cn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                // Print out the values of each column
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader.GetValue(i) + "\t");
+                }
+                Console.WriteLine(); // Move to the next line
+            }
+
+            Console.WriteLine("左輔掀洞庭");
+        }
+    }
+}
