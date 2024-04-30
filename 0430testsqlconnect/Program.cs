ca9d1014e872d495f5de1207631b3ab82ce03eea
iven <iven840320@gmail.com>
2024-04-30 09:25:14 +0800

    加入linq 參考 加入專案檔案。 Here's what's changed:
    We added the System.Data.Linq and System.Linq namespaces.
    We created a DataContext object, which is the LINQ to SQL equivalent of a database connection.
    We defined a Customer class, which represents a row in the Customers table. We decorated the class and its properties with Table and Column attributes to map them to the database table and columns.
    We used the ExecuteQuery method to execute the SQL query and retrieve the results as a collection of Customer objects.
    We looped through the results using a foreach loop and printed out the values of each column using string interpolation.
    Note that in this example, I assumed that the Customers table has columns named CustomerID, CompanyName, ContactName, and Country. You may need to adjust the Customer class and the Console.WriteLine statement to match your actual table schema.

---
+++ 0430testsqlconnect/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+// 定義命名空間THM104_G2
+namespace THM104_G2
+{
+    // 定義內部類別Program
+    internal class Program
+    {
+        // 主要進入點
+        static void Main(string[] args)
+        {
+            // 連接字串指向本地資料庫
+            var connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Northwind;Integrated Security=True";
+            // 建立資料上下文
+            var ctx = new DataContext(connectionString);
+
+            // 執行SQL查詢並將結果映射到Customer類別
+            var customers = ctx.ExecuteQuery<Customer>("SELECT * FROM Customers");
+
+            // 遍歷每個客戶並列印資訊
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"{customer.CustomerID}\t{customer.CompanyName}\t{customer.ContactName}\t{customer.Country}");
+            }
+
+            // 輸出一條訊息
+            Console.WriteLine("Esther好想睡覺");
+        }
+    }
+
+    // 定義與資料庫表格對應的類別
+    [Table(Name = "Customers")]
+    public class Customer
+    {
+        // 定義與資料庫欄位對應的屬性
+        [Column]
+        public string CustomerID { get; set; }
+        [Column]
+        public string CompanyName { get; set; }
+        [Column]
+        public string ContactName { get; set; }
+        [Column]
+        public string Country { get; set; }
+    }
+}
